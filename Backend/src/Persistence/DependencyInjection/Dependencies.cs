27763df07f022aab2071e15b218a6ca5a8cc0795
Lezzess <llezzess@gmail.com>
2021-03-09 09:49:23 +0200

    Add static code analysis

--- Backend/src/Persistence/DependencyInjection/Dependencies.cs
+++ Backend/src/Persistence/DependencyInjection/Dependencies.cs
@@ -1,4 +1,8 @@
-﻿using Core.Services;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using Core.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Services;
 
--- Backend/src/RestApi/DependencyInjection/Dependencies.cs
+++ Backend/src/RestApi/DependencyInjection/Dependencies.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace RestApi.DependencyInjection
 {
